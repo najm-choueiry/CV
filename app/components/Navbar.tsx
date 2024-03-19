6a962e75d7bdae34dcce0eef763656ab83a0f308
najm-choueiry <nachoueiry@outlook.com>
2024-03-19 14:02:57 +0200

    <> Adding the navbar links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,48 @@
+import { useState } from "react"
+import Link from "next/link"
+
 const Navbar = () => {
+
+    const [activeItem, setActiveItem] = useState<string>("")
+
   return (
-    <div>Navbar</div>
+    <div>
+        <span>{activeItem}</span>
+        <div>
+            {
+                activeItem !== 'About' && 
+                (
+                    <Link href="/">
+                <a>
+                    <span>About</span>
+                </a>
+            </Link>
+                )
+            }
+
+            {
+                activeItem !== 'Projects' && 
+                (
+                    <Link href="/projects">
+                <a>
+                    <span>Projects</span>
+                </a>
+            </Link>
+                )
+            }
+
+            {
+                activeItem !== 'Resume' && 
+                (
+                    <Link href="/resume">
+                <a>
+                    <span>Resume</span>
+                </a>
+            </Link>
+                )
+            }
+        </div>
+    </div>
   )
 }
 
