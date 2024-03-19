534e7c93328481c9dd6daa99cafc13bace3a400f
najm-choueiry <nachoueiry@outlook.com>
2024-03-19 14:38:06 +0200

    <> Fixing the link

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,45 +1,38 @@
 'use client'
 
-import { useState } from "react"
-import Link from "next/link"
+import { useState } from "react";
+import Link from "next/link";
 
 const Navbar = () => {
+  const [activeItem, setActiveItem] = useState<string>("");
 
-    const [activeItem, setActiveItem] = useState<string>("")
+  const handleItemClick = (item: string) => {
+    setActiveItem(item);
+  };
 
   return (
     <div>
-        <span className="font-bold text-gray-200">{activeItem}</span>
-        <div>
-            {
-                activeItem !== 'About' && 
-                (
-                    <Link href="/">
-                    <span>About</span>
-            </Link>
-                )
-            }
+      <span className="font-bold text-gray-500">{activeItem}</span>
+      <div className="font-lg flex space-x-3">
+        {activeItem !== "About" && (
+          <Link href="/" onClick={() => handleItemClick("About")}> About </Link>
+        )}
 
-            {
-                activeItem !== 'Projects' && 
-                (
-                    <Link href="/projects">
-                    <span>Projects</span>
-            </Link>
-                )
-            }
+        {activeItem !== "Projects" && (
+          <Link href="/projects" onClick={() => handleItemClick("Projects")} >
+                Projects
+          
+          </Link>
+        )}
 
-            {
-                activeItem !== 'Resume' && 
-                (
-                    <Link href="/resume">
-                    <span>Resume</span>
-            </Link>
-                )
-            }
-        </div>
+        {activeItem !== "Resume" && (
+          <Link href="/resume" onClick={() => handleItemClick("Resume")}>
+            Resume
+          </Link>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
